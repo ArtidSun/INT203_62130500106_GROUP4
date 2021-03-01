882a47b01b5eab00b3749061412e005419c2a140
ArtidSun <sun_vijitpanmai@hotmail.com>
2021-03-01 10:44:06 +0700

    update view photo

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const app = {
     data() {
         return {
             msg: 'Hello, Vue3',
-            tasks: [{ image: 'images/Strike.jpg', article: 'ZGMF-X20A', name: 'Gundam Strike Freedom', done: false },
-            { image: 'images/Exia.jpg', article: 'GN-001', name: 'Gundam Exia', done: false },
-            { image: 'images/Barbatos.jpg', article: 'ASW-G-08', name: 'Gundam Barbatos Lupus Rex', done: false }
+            tasks: [{ image: 'images/Strike.jpg', article: 'ZGMF-X20A', name: 'Gundam Strike Freedom', done: false},
+            { image: 'images/Exia.jpg', article: 'GN-001', name: 'Gundam Exia', done: false},
+            { image: 'images/Barbatos.jpg', article: 'ASW-G-08', name: 'Gundam Barbatos Lupus Rex', done: false}
             ],
             searchOn: false,
             searching: '',
-            currentIndex: 0
+            currentIndex: 0,
+            show: false
         }
     },
     methods: {
@@ -16,6 +17,14 @@ const app = {
             this.tasks[index].done = !this.tasks[index].done;
             this.tasks.done[index] = !this.tasks.done[index];
         },
+
+        showImg(index) {
+            this.show= true;
+            // this.currentIndex = index;
+            console.log(index);
+            console.log(this.show)
+        },
+
         switchsearch() {
             this.searchOn = !this.searchOn;
             this.searching = '';
@@ -26,7 +35,6 @@ const app = {
             return this.tasks.filter(t => t.done).length
         },
         searchingphoto() {
-
             if (this.searching == '') {
                 console.log("test01");
                 return this.tasks;
@@ -39,7 +47,6 @@ const app = {
                 }
                 return menu;
             }
-
         }
     }
 
